a222d0ba7f99d8b4ec55080d6c4314e977bdc191
mohfirdaus22 <thepashter6@gmail.com>
2022-11-25 16:06:20 +0700

    Decrlaration Node for the circular list

--- Exercise3_089/Program.cs
+++ Exercise3_089/Program.cs
@@ -8,6 +8,10 @@ namespace Exercise3_089Exercise_Linked_List_A
 {
     class Node
     {
-       
+      //creates Nodes for the circular nexted list
+      public int rollNumber;
+      public string name
+      public Node next;
     }
+
 }
